9b82438107415ac12c3e745dc6080e8498f4780b
PeiYu <yupeiq1718@gmail.com>
2023-02-16 18:21:31 +0800

    feat: add api env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+  runtimeConfig: {
+    public: {
+      apiToken: 'xVvI6BSSg3HkthKClODN',
+      apiBase: 'https://app.shopia.ai/api/1.1/wf/generate_text_v2'
+    }
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxtjs/device',
