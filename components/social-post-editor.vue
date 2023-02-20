7588c6c8a33333103b6143d8ddd9eec64017a69b
PeiYu <yupeiq1718@gmail.com>
2023-02-20 12:05:04 +0800

    feat: add QuillEditor & addContent

--- components/social-post-editor.vue
+++ components/social-post-editor.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+import { QuillEditor } from '@vueup/vue-quill'
+import '@vueup/vue-quill/dist/vue-quill.snow.css'
+
+const content = ref('')
+
+const addContent = (text:string) => {
+  const htmlText = `<p>${text}</p>`
+  content.value = htmlText.concat(content.value)
+}
+
+defineExpose({ addContent })
+
+</script>
+
 <template>
-  <section class="bg-gray-900 shadow-md rounded-lg p-8 mx-2" />
+  <section class="bg-gray-900 shadow-md rounded-lg p-8 mx-2 relative">
+    <section class="h-[calc(100%-3rem)] bg-white">
+      <QuillEditor v-model:content="content" content-type="html" class="bg-white" />
+    </section>
+  </section>
 </template>
