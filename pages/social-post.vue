449de5a840df683abfd5c1bcbeb5accb1aff6a3f
PeiYu <yupeiq1718@gmail.com>
2023-02-20 11:18:06 +0800

    feat: modify social post css

--- pages/social-post.vue
+++ pages/social-post.vue
@@ -1,7 +1,7 @@
 
 <template>
-  <div class="container grid grid-cols-3 px-5 py-24 mx-auto">
-    <SocialPostQuery class="col-span-1" />
-    <SocialPostEditor class="col-span-2" />
+  <div class="container grid grid-cols-6 px-4 py-2 lg:py-24 mx-auto lg:overflow-hidden">
+    <SocialPostQuery class="col-span-6 lg:col-span-2 m-2" />
+    <SocialPostEditor class="col-span-6 lg:col-span-4 m-2 overflow-hidden" />
   </div>
 </template>
