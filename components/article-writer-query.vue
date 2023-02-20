372224757f50d58f84d51acd2ea7ff5ebefa9e98
PeiYu <yupeiq1718@gmail.com>
2023-02-20 19:03:41 +0800

    fix: modify loading & UI

--- components/article-writer-query.vue
+++ components/article-writer-query.vue
@@ -11,92 +11,94 @@ const setArticleTitle = (text: string) => {
 
 const titleList = ref<string[]>([])
 
-const isLoading = ref(false)
+const isLoading = reactive({
+  titleList: false,
+  articleTitle: false
+})
 
 const { createArticleTitle, createArticleOutline } = useApi()
 
 const generateArticleTitle = async (text: string) => {
-  isLoading.value = true
+  isLoading.titleList = true
   const { response } = await createArticleTitle(text)
   const data = response.generated_text.split('\n').map(title => title.split('. ')[1] || title)
   titleList.value = data
-  description.value = ''
-  isLoading.value = false
+  isLoading.titleList = false
 }
 const generateArticleOutline = async (text: string) => {
-  isLoading.value = true
+  isLoading.articleTitle = true
   const { response } = await createArticleOutline(text)
   const data = response.generated_text.split('\n').map(title => title.split('. ')[1] || title)
   titleList.value = data
   description.value = ''
-  isLoading.value = false
+  titleList.value = []
+  switchValue.value = 'Output'
+  isLoading.articleTitle = false
 }
 
 </script>
 
 <template>
-  <section class="bg-gray-900 shadow-md rounded-lg p-8 mx-2 flex flex-col relative">
-    <div class="absolue">
-      <header class="flex flex-col">
-        <BaseSwitch
-          v-model:value="switchValue"
-          :title-list="['Input', 'Output']"
-          class="mb-8"
+  <section class="bg-gray-900 shadow-md rounded-lg p-8 ml-2 flex flex-col justify-start">
+    <header class="flex flex-col">
+      <BaseSwitch
+        v-model:value="switchValue"
+        :title-list="['Input', 'Output']"
+        class="mb-8"
+      />
+    </header>
+    <article
+      v-if="switchValue === 'Input'"
+      class="w-full relative lg:overflow-auto pr-2"
+    >
+      <header class="flex items-end">
+        <BaseTextarea
+          v-model:value="description"
+          title="Describe your Article (required)"
+          :disabled="isLoading.titleList"
+          class="flex-1 mr-1"
         />
-      </header>
-      <article
-        v-if="switchValue === 'Input'"
-        class="w-full"
-      >
-        <header class="flex items-end">
-          <BaseTextarea
-            v-model:value="description"
-            title="Describe your Article (required)"
-            :disabled="isLoading"
-            class="flex-1 mr-1"
-          />
-          <br class="my-4">
-          <button
-            class="text-white bg-green-500 border-0 py-5 px-2 m-2 focus:outline-none hover:bg-green-600 rounded text-lg flex justify-center items-center"
-            @click="generateArticleTitle(description)"
-          >
-            <span v-if="isLoading" class="mx-2 animate-spin">
-              <IconLoading />
-            </span>
-            <span v-else>Generate</span>
-          </button>
-        </header>
         <br class="my-4">
-        <article>
-          <button
-            v-for="title of titleList"
-            :key="title"
-            class="text-white bg-gray-400 border-0 py-2 px-3 my-2 focus:outline-none hover:bg-green-600 rounded text-md flex justify-center items-center"
-            @click="setArticleTitle(title)"
-          >
-            {{ title }}
-          </button>
-        </article>
-        <br class="my-2">
-        <footer>
-          <BaseTextarea
-            v-model:value="articleTitle"
-            title="Describe your Article (required)"
-            :disabled="isLoading"
-            class="flex-1 mr-1"
-          />
-          <br class="my-2">
-          <button
-            class="w-full text-white bg-green-500 border-0 py-4 px-6 focus:outline-none hover:bg-green-600 rounded text-lg flex justify-center"
-            @click="generateArticleOutline(description)"
-          >
-            <span v-if="isLoading" class="mx-2 animate-spin">
-              <IconLoading />
-            </span>
-            <span v-else>Use As Title</span>
-          </button>
-        </footer>
+        <button
+          class="text-white bg-green-500 border-0 py-5 px-2 m-2 focus:outline-none hover:bg-green-600 rounded text-lg flex justify-center items-center"
+          @click="generateArticleTitle(description)"
+        >
+          <span v-if="isLoading.titleList" class="mx-2 animate-spin">
+            <IconLoading />
+          </span>
+          <span v-else>Generate</span>
+        </button>
+      </header>
+      <br class="my-4">
+      <article>
+        <button
+          v-for="title of titleList"
+          :key="title"
+          class="text-white bg-gray-400 border-0 py-2 px-3 my-2 focus:outline-none hover:bg-green-600 rounded text-md flex justify-center items-center"
+          @click="setArticleTitle(title)"
+        >
+          {{ title }}
+        </button>
       </article>
-    </div>
+      <br class="my-2">
+      <footer>
+        <BaseTextarea
+          v-model:value="articleTitle"
+          title="Describe your Article (required)"
+          :disabled="isLoading.articleTitle"
+          class="flex-1 mr-1"
+        />
+        <br class="my-2">
+        <button
+          class="w-full text-white bg-green-500 border-0 py-4 px-6 focus:outline-none hover:bg-green-600 rounded text-lg flex justify-center"
+          @click="generateArticleOutline(description)"
+        >
+          <span v-if="isLoading.articleTitle" class="mx-2 animate-spin">
+            <IconLoading />
+          </span>
+          <span v-else>Use As Title</span>
+        </button>
+      </footer>
+    </article>
   </section>
 </template>
