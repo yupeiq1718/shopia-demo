02ed523f047079c9093f36b434d3b6c9b613afa8
PeiYu <yupeiq1718@gmail.com>
2023-02-24 10:38:55 +0800

    feat: modify card style

--- components/base-card.vue
+++ components/base-card.vue
@@ -19,17 +19,17 @@ const emits = defineEmits<Emits>()
 
 <template>
   <div class="bg-gray-800 rounded border border-gray-800 pt-4 px-4 mb-4">
-    <p class="mb-4 text-gray-400 text-md">
+    <p class="mb-2 text-gray-400 text-md">
       {{ props.title }}
     </p>
     <span class="inline-flex w-full justify-end">
-      <a class="text-gray-500">
+      <a class="text-gray-500 ml-2">
         <IconDelete
           class="fill-gray-400 cursor-pointer hover:fill-green-400"
           @click="emits('remove')"
         />
       </a>
-      <a class="text-gray-500">
+      <a class="text-gray-500 ml-2">
         <IconPlay
           class="fill-gray-400 cursor-pointer hover:fill-green-400"
           @click="emits('add')"
