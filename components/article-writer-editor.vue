08dda20331628b0efa0ad0e606db2db21ac1d3a0
PeiYu <yupeiq1718@gmail.com>
2023-02-24 18:53:56 +0800

    feat: modify setContent

--- components/article-writer-editor.vue
+++ components/article-writer-editor.vue
@@ -37,10 +37,18 @@ const switchSectionTitle = (firstIndex: number, secondIndex: number) => {
 
 const content = ref('')
 
-const addContent = (text: string) => {
-  const htmlText = `<p>${text}</p>`
-  content.value = content.value.concat(htmlText)
+const setContent = (text: string) => {
+  content.value = text
 }
+
+// const insertContent = ({ queryString, insertString }: {
+//   queryString: string,
+//   insertString: string
+// }) => {
+//   const index = content.value.indexOf(queryString)
+//   console.log(index)
+//   console.log(insertString)
+// }
 
 const { createArticleHeader, createArticleSection, createArticleFooter } = useApi()
 
@@ -49,25 +57,38 @@ const isLoading = reactive({
 })
 
 const generateArticle = async () => {
-  isLoading.article = true
-  addContent(`<h1>${props.articleTitle}</h1>`)
+  try {
+    isLoading.article = true
+    const contentList = []
+    contentList.push(`<h1>${props.articleTitle}</h1>`)
+    contentList.push('<h2>簡介</h2>')
 
-  const titleListString = sectionTitleList.value.join('\n')
-  const responseHeader = await createArticleHeader(titleListString)
-  const header = responseHeader.response.generated_text
-  addContent(`<h2><strong>介紹</strong></h2><br><p>${header}</p>`)
+    sectionTitleList.value.forEach(async (title, index) => {
+      contentList.push(`<h2>${title}</h2>`)
+      const responseSection = await createArticleSection(title)
+      const section = responseSection.response.generated_text
+      contentList[index + 2] = `<h2>${title}</h2><br><p>${section}</p>`
+    })
 
-  sectionTitleList.value.forEach(async (title) => {
-    const responseSection = await createArticleSection(title)
-    const section = responseSection.response.generated_text
-    addContent(`<h2><strong>${title}</strong></h2><br><p>${section}</p>`)
-  })
+    contentList.push('<h2>結論</h2>')
+    setContent(contentList.join('<br>'))
 
-  const responseFooter = await createArticleFooter(header)
-  const footer = responseFooter.response.generated_text
-  addContent(`<h2><strong>結論</strong></h2><br><p>${footer}</p>`)
+    const titleListString = sectionTitleList.value.join('\n')
+    const responseHeader = await createArticleHeader(titleListString)
+    const header = responseHeader.response.generated_text
+    contentList[1] = `<h2>簡介</h2><br><p>${header}</p>`
+    setContent(contentList.join('<br>'))
 
-  isLoading.article = false
+    const responseFooter = await createArticleFooter(header)
+    const footer = responseFooter.response.generated_text
+    contentList[sectionTitleList.value.length + 2] = `<h2>結論</h2><br><p>${footer}</p>`
+    setContent(contentList.join('<br>'))
+    console.log(contentList.join('<br>'))
+  } catch (error) {
+    console.error(error)
+  } finally {
+    isLoading.article = false
+  }
 }
 
 defineExpose({ createSectionTitle })
@@ -88,7 +109,7 @@ defineExpose({ createSectionTitle })
       class="w-full relative lg:overflow-auto pr-2"
     >
       <header class="border border-gray-800 rounded bg-gray-800 text-white text-xl text-center py-4 px-4 mb-4">
-        {{ props.articleTitle }}
+        {{ props.articleTitle || '(Title)' }}
       </header>
       <article>
         <section class="border border-gray-800 rounded bg-gray-800 text-white  py-4 px-4 mb-4">
